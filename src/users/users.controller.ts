96b8f785ac01e90ea6d5f33293bb2168ec367057
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-19 22:23:24 +0530

    add barebones routes (lesson 2)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+// decorators are predefined functions that run auto when called
+@Controller('users') // /users
+export class UsersController {
+  /*
+    GET /users or /users?role=value
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+   */
+  @Get()
+  findAll(@Query('role') role?: 'intern' | 'engineer' | 'admin ') {
+    //since role is optional, therefore ? after role
+    return ['list of users'];
+  }
+
+  //@Get("products") /users/products
+  //waterfall of routes -> /users/:id and /users/product (if id routes is placed before product then anything after /users will be read as id) (therefore product should be placed before id)
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() man: {}) {
+    return man;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
